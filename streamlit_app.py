756b294b27bbb7e189b515e8086e6de4ae9c31c0
shaanramnah <shaanramnah@hotmail.co.uk>
2025-05-21 15:29:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ ingredients_list = st.multiselect(
 if ingredients_list:
   ingredients_string = ''
 
-    for fruit_chosen in ingredients_list: 
+  for fruit_chosen in ingredients_list: 
       ingredients_string += fruit_chosen + ' '
 
     #st.write(ingredients_string)
