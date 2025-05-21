126309dad1b4beff895ea645792fd2a6bc02d3b4
shaanramnah <shaanramnah@hotmail.co.uk>
2025-05-21 17:01:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
+
 
 # Write directly to the app
 st.title(f"Customise Your Smoothie :cup_with_straw: {st.__version__}")
