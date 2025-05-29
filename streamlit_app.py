6eae6569aadadafb43d8beecce19fc9c69abad51
shaanramnah <shaanramnah@hotmail.co.uk>
2025-05-29 14:47:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,10 @@ if ingredients_list:
     st.write(my_insert_stmt)
     #st.stop()
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
+  
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
